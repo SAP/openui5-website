7a9fa084e958158bcf017b00ffe5503b594b0998
UI5.Bot <noreply@sap.com>
2020-07-09 18:16:55 +0000

    Automatic deploy to GitHub Pages: UI5con (Build 303442)

--- ui5con/onair2020/_next/static/v05_Jl8FzOGj9rhbFsYyL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fcss\u002F6af7702b788d08b9cc8a.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ ui5con/onair2020/_next/static/vo9U3zJSn4WhHeWvuhVTu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fcss\u002F6af7702b788d08b9cc8a.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
