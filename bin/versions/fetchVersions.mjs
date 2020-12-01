39ae936ce7a078ff7a382a751c31d9bba8d851a2
Pavel Kornev <pavel.kornev@sap.com>
2020-12-01 13:19:16 +0100

    Replace version type with eom & cleanup

--- bin/versions/fetchVersions.mjs
+++ bin/versions/fetchVersions.mjs
@@ -3,11 +3,12 @@ import { writeFileSync } from "fs";
 import path from "path";
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const FILE_PATH = path.resolve(__dirname, "../data/pages/releases/versions.json");
+const FILE_PATH = path.resolve(__dirname, "../../data/pages/releases/versions.json");
+const VERSIONS_URL = "https://openui5versiontracker.cfapps.eu10.hana.ondemand.com/OpenUI5ReleasesInfo";
 
 (async () => {
   try {
-    const response = await fetch("https://openui5versiontracker.cfapps.eu10.hana.ondemand.com/OpenUI5ReleasesInfo");
+    const response = await fetch(VERSIONS_URL);
     const json = await response.json();
     writeFileSync(FILE_PATH, JSON.stringify(json, null, 2));
     console.log(`File with latest versions information is successfuly saved in ${FILE_PATH}`);
