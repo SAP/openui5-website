c06dc9af29ace196b6865a7847586d575293e500
UI5.Bot <noreply@sap.com>
2021-04-07 05:41:33 +0000

    Automatic deploy to GitHub Pages: UI5con (Build 623710)

---
+++ ui5con/onair2021/_next/static/CDEDN9G2cGOb5KcA85PFX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F813608a12537c9910407.css","static\u002Fchunks\u002Fpages\u002Findex-cceea7f6df33ae2faa00.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8b6d24716d82ca3ee4bf.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
