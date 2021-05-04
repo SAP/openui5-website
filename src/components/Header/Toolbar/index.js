3e30559d0684481d73f09d495293befbf178af16
Pavel Kornev <pavel.kornev@sap.com>
2021-05-04 10:41:00 +0200

    Header/Navigation animations

--- src/components/Header/Toolbar/index.js
+++ src/components/Header/Toolbar/index.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Link } from "gatsby";
 import styles from "./styles.module.css";
 import classnames from "classnames";
-import { useLocation } from "@reach/router"
+import { useLocation } from "@reach/router";
+import { useMediaQuery } from "@react-hook/media-query";
 
 import Hamburger from "../Hamburger";
 import Logo from "../../Logo";
@@ -11,8 +12,13 @@ import Navigation from "../Navigation";
 import GitHub from "../GitHub";
 
 
-const ToolbarItem = ({ mod, children }) => (
-    <div className={classnames(styles.ToolbarItem, styles[`mod_${mod}`])}>
+const ToolbarItem = ({ mod, className, children }) => (
+    <div
+        className={classnames(
+            styles.ToolbarItem, styles[`mod_${mod}`],
+            className
+        )}
+    >
         { children }
     </div>
 );
@@ -20,6 +26,7 @@ const ToolbarItem = ({ mod, children }) => (
 const Toolbar = ({ version }) => {
     const [isOpen, setIsOpen] = useState(false);
     const location = useLocation();
+    const isMobile = useMediaQuery("(max-width: 767px)");
 
     function onHamburgerClick() {
         setIsOpen(!isOpen);
@@ -27,19 +34,32 @@ const Toolbar = ({ version }) => {
 
     useEffect(() => {
         setIsOpen(false);
-    }, [location]);
+    }, [location, isMobile]);
+
+    useEffect(() => {
+        const htmlClassList = document.documentElement.classList;
+        if (isOpen) {
+            htmlClassList.add("noScroll");
+        } else {
+            htmlClassList.remove("noScroll");
+        }
+    }, [isOpen]);
 
     return (
         <div
             className={classnames(
                 styles.Toolbar,
-                isOpen ? styles.state_open : null,
+                isOpen ? styles.view_open : null
             )}
         >
-            <ToolbarItem mod="hamburger"><Hamburger open={isOpen} onClick={onHamburgerClick} /></ToolbarItem>
+            {
+                isMobile
+                    ? <ToolbarItem mod="hamburger"><Hamburger open={isOpen} onClick={onHamburgerClick} /></ToolbarItem>
+                    : null
+            }
             <ToolbarItem mod="logo"><Link to="/" style={{ display: 'inline-block' }}><Logo /></Link></ToolbarItem>
             <ToolbarItem mod="version"><Version>{version}</Version></ToolbarItem>
-            <ToolbarItem mod="navigation"><Navigation /></ToolbarItem>
+            <ToolbarItem mod="navigation"><Navigation className={styles.Navigation} /></ToolbarItem>
             <ToolbarItem mod="github"><GitHub /></ToolbarItem>
         </div>
     );
