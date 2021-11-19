f3f4c4c7663d1cefea4084e122d8c3968c47ea98
Pavel Kornev <pavel.kornev@sap.com>
2021-11-19 12:35:02 +0100

    Preload events icons (#357)

--- src/components/Event/AddEvent.js
+++ src/components/Event/AddEvent.js
@@ -3,11 +3,7 @@ import classnames from "classnames";
 import styles from "./styles.module.css";
 import loadable from '@loadable/component'
 
-loadable(() => import("@ui5/webcomponents-icons/dist/add"))
-
-const IconComponent = loadable(() => import("@ui5/webcomponents-react/lib/Icon"), {
-  resolveComponent: (components) => components.Icon,
-})
+const AddIcon = loadable(() => import("./AddIcon"))
 
 const AddEvent = () => (
   <a
@@ -18,7 +14,7 @@ const AddEvent = () => (
     href="mailto:openui5@sap.com?subject=Add%20event%20to%20openui5.org&body=Title:%0DDate:%0DLocation:%0DURL:"
   >
     <div className={styles.AddEvent}>
-      <div className={styles.AddEventIcon}><IconComponent name="add" /></div>
+      <div className={styles.AddEventIcon}><AddIcon /></div>
       <div className={styles.AddEventTitle}>Add Event</div>
       <div className={styles.AddEventText}>Are you or someone you know speaking about UI5 at an upcoming meetup or conference? Let us know</div>
     </div>
