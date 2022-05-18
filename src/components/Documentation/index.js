5e083999c27632dd2be5f9a4157f73dcd63a12a9
Evgeny Segeda <evgeny.segeda@sap.com>
2022-05-18 14:22:15 +0200

    Update URL

--- src/components/Documentation/index.js
+++ src/components/Documentation/index.js
@@ -9,12 +9,12 @@ const Documentation = () => {
             <div className={styles.Documentation__col1}>
                 <div className={styles.Documentation__title}>The Demo Kit is the central place to understand and learn OpenUI5</div>
                 <div className={styles.Documentation__text}>
-                    It contains the official <Link href="https://openui5.hana.ondemand.com/#/topic" target="_blank" rel="noopener">documentation</Link>,
-                    &nbsp;<Link href="https://openui5.hana.ondemand.com/#/api" target="_blank" rel="noopener">the API Reference</Link>,
-                    &nbsp;as well as <Link href="https://openui5.hana.ondemand.com/#/controls" target="_blank" rel="noopener">code samples</Link>
+                    It contains the official <Link href="https://sdk.openui5.org/#/topic" target="_blank" rel="noopener">documentation</Link>,
+                    &nbsp;<Link href="https://sdk.openui5.org/#/api" target="_blank" rel="noopener">the API Reference</Link>,
+                    &nbsp;as well as <Link href="https://sdk.openui5.org/#/controls" target="_blank" rel="noopener">code samples</Link>
                     &nbsp;for many UI controls to demonstrate their use.
                 </div>
-                <Button href="https://openui5.hana.ondemand.com/" rel="noopener" target="_blank" size="2">Browse Demo Kit</Button>
+                <Button href="https://sdk.openui5.org/" rel="noopener" target="_blank" size="2">Browse Demo Kit</Button>
             </div>
             <div className={styles.Documentation__col2}></div>
         </div>
