2f1620908b68299a561d6be1b36f4709c2180ceb
UI5.Bot <noreply@sap.com>
2023-02-20 11:21:20 +0000

    Automatic deploy to GitHub Pages: UI5con (Build 1522175)

--- ui5con/germany2023/js/main.js
+++ ui5con/germany2023/js/main.js
@@ -120,3 +120,115 @@ var main = new Vue({
     }
   },
 });
+
+var aboutTheTeam = new Vue({
+  el: '#team',
+  data() {
+    return {
+      team: [
+        {
+          name: 'Uliana Caceres',
+          city: 'Munich',
+          picture: 'images/team/uliana-caceres.png',
+          linkedin: 'https://www.linkedin.com/in/ulianaa/',
+          github: 'https://github.com/UlianaMunich',
+        },
+        {
+          name: 'Eniela Vela',
+          city: 'Montreal',
+          picture: 'images/team/eniela.jpg',
+          linkedin: 'https://ca.linkedin.com/in/eniela-vela-67aba3135',
+        },
+        {
+          name: 'Victoria Maslova',
+          city: 'Vancouver',
+          picture: 'images/team/victoria-maslova.jpeg',
+          linkedin: 'https://www.linkedin.com/in/victoria-maslova/',
+        },
+        {
+          name: 'Margot Wollny',
+          city: 'Heidelberg',
+          picture: 'images/team/margot-wollny.png',
+          linkedin: 'https://www.linkedin.com/in/margot-wollny-864ba96',
+        },
+        {
+          name: 'Inna Atanasova',
+          city: 'Montreal',
+          picture: 'images/team/Inna.jpg',
+          linkedin: 'https://www.linkedin.com/in/inna-atanasova-654933137/',
+          github: 'https://github.com/InnaAtanasova',
+        },
+        {
+          name: 'Frederic Berg',
+          city: 'Walldorf',
+          picture: 'images/team/frederic_berg.jpg',
+          linkedin: 'https://www.linkedin.com/in/frederic-berg-2433b388/',
+          twitter: 'https://twitter.com/frdrcbrg',
+        },
+        {
+          name: 'Fabian Tempel',
+          city: 'Potsdam',
+          picture: 'images/team/fabian-tempel.jpg',
+          linkedin: 'https://www.linkedin.com/in/fabian-tempel/',
+          github: 'https://github.com/trilodge',
+          twitter: 'https://www.twitter.com/trilodge',
+        },
+        {
+          name: 'Peter Muessig',
+          city: 'Walldorf',
+          picture: 'images/team/peter-muessig.jpg',
+          linkedin: 'https://www.linkedin.com/in/peter-muessig-7b40576/',
+          github: 'https://github.com/petermuessig',
+          twitter: 'https://twitter.com/pmuessig',
+        },
+        {
+          name: 'Andreas Kunz',
+          city: 'Walldorf',
+          picture: 'images/committee/andreas-kunz.jpg',
+          linkedin: 'https://www.linkedin.com/in/andreas-kunz-dev',
+          github: 'https://github.com/akudev',
+          twitter: 'https://twitter.com/aku_dev',
+        },
+        {
+          name: 'Stefan Beck',
+          city: 'Walldorf',
+          picture: 'images/committee/stefan-beck.jpg',
+          linkedin: 'https://linkedin.com/in/stefan-beck-a9319a82',
+          twitter: 'https://twitter.com/StefanBeck3',
+        },
+        {
+          name: 'Sebastian Moreno',
+          city: 'Walldorf',
+          picture: 'images/team/sebastian-moreno.jpg',
+          linkedin: 'https://www.linkedin.com/in/sebastianmorenoagredo/',
+        },
+        {
+          name: 'Arthur Trauter',
+          city: 'Walldorf',
+          picture: 'images/team/arthur-trauter.png',
+          twitter: 'https://twitter.com/ArthurTrauter',
+          github: 'https://github.com/ArthurTrauter'
+        },
+        {
+          name: 'Dorota Josenhans',
+          city: 'Walldorf',
+          picture: 'images/team/dorota-josenhans.png',
+          linkedin: 'https://www.linkedin.com/in/dorota-josenhans-b5615124/',
+        },
+        {
+          name: 'Oliver Müller',
+          city: 'Walldorf',
+          picture: 'images/team/oliver-mueller.jpg',
+          linkedin: 'https://www.linkedin.com/in/oliver-m%C3%BCller-648702a8/',
+        },
+        {
+          name: 'Alexander Rauh',
+          city: 'Walldorf',
+          picture: 'images/team/alex-rauh.jpg',
+          linkedin: 'https://www.linkedin.com/in/alexander-rauh-899579168/',
+        },
+      ],
+    };
+  },
+});
+
