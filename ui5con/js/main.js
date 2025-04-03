5a43946ab25df899da71c908ec05975c4df28b96
UI5.Bot <noreply@sap.com>
2025-04-03 17:40:39 +0000

    Automatic deploy to GitHub Pages: UI5con (Run #186, Build 12561514, Commit e872648670c9653231781040a6c1f936a430d34a)

--- ui5con/js/main.js
+++ ui5con/js/main.js
@@ -288,7 +288,27 @@ var main = new Vue({
     ],
     activeSpeakers: null,
     lastFocussedElementID: '',
+    speakers: [],
+    filter: "all",
+    activeSpeakers: null,
+    lineup: [],
+    proposalLineup: [],
+    formattedLineup: [],
    }
+  },
+  mounted() {
+    axios
+    .get('https://ui5con.cfapps.eu12.hana.ondemand.com/api/speaker/lineup')
+    .then(response => {
+      this.speakers = this.formatAndShuffleSpeakersArray(response.data).slice(0, 6);
+    });
+
+    axios
+    .get('https://ui5con.cfapps.eu12.hana.ondemand.com/api/proposal/lineup')
+    .then(response => {
+      this.lineup = response;
+      this.formattedLineup = this.formatLineup();
+    });
   },
   methods: {
     openSpeakerInfoModal(speakers, id) {
@@ -369,19 +389,213 @@ var main = new Vue({
         }
       }
     },
+    formatTwitterLink(handle) {
+      if (!handle.startsWith('https:')) {
+        return "https://twitter.com/" + handle;
+      }
+    },
+    formatLinkedInLink(handle) {
+      if (!handle.startsWith('https:')) {
+        return "https://www.linkedin.com/in/" + handle;
+      }
+    },
+    formatMastodonLink(handle) {
+      if(!handle.startsWith('https:')) {
+        if (handle.includes('@saptodon.org')) {
+          return 'https://saptodon.org/' + handle.replace('@saptodon.org', '');
+        }
+
+        return 'https://saptodon.org/' + handle;
+      }
+    },
+    formatBlueskyLink(handle) {
+      if(!handle.startsWith('https:')) {
+        return 'https://bsky.app/profile/' + handle.replace('@', '');
+      }
+    },
+    shuffleSpeakersArray(array) {
+      const newArray = [...array]
+      const filteredArray = newArray.filter((el) => el.hasPhoto);
+      const length = filteredArray.length
+
+      for (let start = 0; start < length; start++) {
+        const randomPosition = Math.floor((filteredArray.length - start) * Math.random());
+        const randomItem = filteredArray.splice(randomPosition, 1);
+        filteredArray.push(...randomItem);
+      }
+
+      return filteredArray;
+    },
+    formatAndShuffleSpeakersArray(array) {
+      const formattedArray = this.formatSpeakersArray(array);
+      return this.shuffleSpeakersArray(formattedArray);
+    },
+    formatSpeakersArray(array) {
+      const newArray = [...array]
+      const formattedArray = newArray.map(speaker => {
+        const fullName = speaker.firstName + ' ' + speaker.lastName;
+
+        if(speaker.twitterHandle) {
+          speaker.twitterHandle = this.formatTwitterLink(speaker.twitterHandle);
+        }
+
+        if(speaker.linkedInUrl) {
+          speaker.linkedInUrl = this.formatLinkedInLink(speaker.linkedInUrl);
+        }
+
+        if(speaker.mastodonHandle) {
+          speaker.mastodonHandle = this.formatMastodonLink(speaker.mastodonHandle);
+        }
+
+        if(speaker.blueskyHandle) {
+          speaker.blueskyHandle = this.formatBlueskyLink(speaker.blueskyHandle);
+        }
+
+        return {
+          ...speaker,
+          fullName: fullName,
+          showMore: false
+        }
+      });
+
+      return formattedArray;
+    },
+    onFilterChange($event) {
+      this.filter = $event.target.value;
+      this.formattedLineup = this.formatLineup();
+    },
+    formatLineup() {
+      const tempLineUp = this.lineup.data.map((session) => {
+        session.speakers.map((speaker) => {
+          if(speaker.twitterHandle) {
+            speaker.twitterHandle = this.formatTwitterLink(speaker.twitterHandle);
+          }
+
+          if(speaker.linkedInUrl) {
+            speaker.linkedInUrl = this.formatLinkedInLink(speaker.linkedInUrl);
+          }
+
+          if(speaker.mastodonHandle) {
+            speaker.mastodonHandle = this.formatMastodonLink(speaker.mastodonHandle);
+          }
+
+          if(speaker.blueskyHandle) {
+            speaker.blueskyHandle = this.formatBlueskyLink(speaker.blueskyHandle);
+          }
+        });
+
+        // let start = session.startTime;
+        // let end = session.endTime;
+
+        let start = '9:00';
+        let end = '10:00';
+
+        let tempStart = start.substring(0, start.indexOf(":"));
+        let tempEnd = end.substring(0, end.indexOf(":"));
+
+        if (tempStart.length == 1 && !tempStart.startsWith("0")) {
+          start = "0" + start;
+        }
+
+        if (tempEnd.length == 1 && !tempEnd.startsWith("0")) {
+          end = "0" + end;
+        }
+
+        let newStartTime = "2025-07-08T" + start + ":00.000+02:00";
+        let newEndTime = "2025-07-08T" + end + ":00.000+02:00";
+
+
+        let timeNow = new Date().toISOString();
+        let sessionTimeStart = new Date(newStartTime).toISOString();
+        let sessionTimeEnd = new Date(newEndTime).toISOString();
+        let sessionLiveStatus = false;
+
+        if (timeNow > sessionTimeStart && timeNow < sessionTimeEnd) {
+          sessionLiveStatus = true;
+        }
+
+        // TO BE REMOVED WHEN WE HAVE THE ROOMS!!!!!!!!!!
+        let location = 'Audimax';
+
+        if (session.type.includes('presentation')) {
+          location = 'W1/W2'
+        } else if (session.type.includes('hands')) {
+          location = 'W32'
+        }
+
+        return {
+          ...session,
+          startTime: newStartTime,
+          endTime: newEndTime,
+          isLive: sessionLiveStatus,
+          location: location
+        };
+      });
+
+      const sortedScheduleTemp = tempLineUp.sort((a, b) =>
+        luxon.DateTime.fromISO(a.startTime) -
+        luxon.DateTime.fromISO(b.startTime)
+      );
+
+      this.expertCornerLineupUnsorted = sortedScheduleTemp.filter(
+        (schedule) => schedule.type.includes("expert")
+      );
+
+      const sortedSchedule = sortedScheduleTemp.filter(
+        (schedule) => !schedule.type.includes("expert")
+      );
+
+      if (this.filter === "all") {
+        return sortedSchedule;
+      } else if (this.filter === "talks") {
+        return sortedSchedule.filter((schedule) =>
+          schedule.type.includes("presentation")
+        );
+      } else if (this.filter === "workshops") {
+        return sortedSchedule.filter((schedule) =>
+          schedule.type.includes("hands")
+        );
+      } else if (this.filter === "audimax") {
+        return sortedSchedule.filter((schedule) =>
+          schedule.location.toLowerCase() === "audimax"
+        );
+      } else if (this.filter === "w1") {
+        return sortedSchedule.filter((schedule) =>
+          schedule.location.toLowerCase().includes("w1")
+        );
+      } else if (this.filter === "w3") {
+        return sortedSchedule.filter((schedule) =>
+          schedule.location.toLowerCase().includes("w3")
+        );
+      } else if (this.filter === "beginner") {
+        return sortedSchedule.filter((schedule) =>
+          schedule.proficiencyLevel === "beginner"
+        );
+      } else if (this.filter === "intermediate") {
+        return sortedSchedule.filter((schedule) =>
+          schedule.proficiencyLevel === "intermediate"
+        );
+      } else if (this.filter === "advanced") {
+        return sortedSchedule.filter((schedule) =>
+          schedule.proficiencyLevel === "advanced"
+        );
+      } else {
+        return sortedSchedule;
+      }
+    },
   },
   filters: {
     formatLocation: function (value) {
       if (value) {
-        if (value.includes("audimax")) {
+        if (value.toLowerCase().includes("audimax")) {
           return "A";
-        } else if (value.includes("w1") || value.includes("w2")) {
+        } else if (value.toLowerCase().includes("w1") || value.toLowerCase().includes("w2")) {
           return "WS1/2";
-        } else if (value.includes("w3")) {
+        } else if (value.toLowerCase().includes("w3")) {
           return "WS3"
-        } else if (value.includes("expert")) {
+        } else if (value.toLowerCase().includes("expert")) {
           return "EC"
-        } else if (value.includes("canteen")) {
+        } else if (value.toLowerCase().includes("canteen")) {
           return "CA"
         } else {
           return value;
@@ -389,9 +603,24 @@ var main = new Vue({
       }
     },
     formatLevel: function (value) {
-      if (value) {
-          return "E";
+      if (!value) return ''; 
+      return value.charAt(0).toUpperCase();
+    },
+    trimTime: function (value) {
+      let time = value.substring(value.indexOf("T") + 1);
+      let timeSplit = time.split(":");
+      let hour = timeSplit[0].startsWith("0")
+        ? timeSplit[0].replace(/^0+/, "")
+        : timeSplit[0];
+      return hour + ":" + timeSplit[1];
+    },
+    convertTime: function (value, eventTime) {
+      if (eventTime === "local") {
+        return luxon.DateTime.fromISO(value)
+          .toLocal()
+          .toISO({ suppressMilliseconds: true });
       }
+      return value;
     },
   }
 });
